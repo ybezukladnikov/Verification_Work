6f0927336cc4d157622fd0ae6f63473420a1ebaa
Yaroslav <yaros6666@yandex.ru>
2022-08-02 11:06:21 +0300

    Created ShowArray

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿string [] CreateArray()
+{
+    Console.Write("Input size array: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string [] newArray = new string[size];
+    
+    
+    
+    
+    return newArray;
+}
+
+void ShowArray(string [] array)
+{
+    Console.WriteLine("Created array: ");
+    Console.Write("[");
+    for(int i =0; i<array.Length; i++)
+    {
+        if(i == array.Length-1)
+        {
+            Console.Write(array[i]);
+        }
+        else
+            Console.Write(array[i] + ", ");
+
+    }
+    Console.Write("]");
+        
+}
+
+string [] newArray = CreateArray();
+
+ShowArray(newArray);
