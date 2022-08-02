845f3dd1b1010a81dc8e4d3e087c09412a8dc430
Yaroslav <yaros6666@yandex.ru>
2022-08-02 12:10:07 +0300

    Created CreatedArray

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,12 +1,18 @@
-﻿string [] CreateArray()
+﻿string [] CreateArray(int size, string characterSet, int minSizeRow, int maxSizeRow)
 {
-    Console.Write("Input size array: ");
-    int size = Convert.ToInt32(Console.ReadLine());
+    
     string [] newArray = new string[size];
     
-    
-    
-    
+    for(int j=0; j<size; j++)
+    {
+        int rand = new Random().Next(minSizeRow,maxSizeRow+1);
+        for (int i = 0; i<rand; i++)
+        {
+            newArray[j] = newArray[j] + characterSet[new Random().Next(0,characterSet.Length)];
+        }
+    }
+
+
     return newArray;
 }
 
@@ -28,6 +34,21 @@ void ShowArray(string [] array)
         
 }
 
-string [] newArray = CreateArray();
+
+
+
+Console.Write("Input size array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input characterSet: ");
+string characterSet = Console.ReadLine();
+
+Console.Write("Input min Size Row array: ");
+int minSizeRow = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input max Size Row array: ");
+int maxSizeRow = Convert.ToInt32(Console.ReadLine());
+
+string [] newArray = CreateArray(size, characterSet, minSizeRow,maxSizeRow);
 
 ShowArray(newArray);
